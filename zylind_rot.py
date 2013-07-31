3ba7432a274d5178ee4084d1e84698aa2f5180d9
CRoduner <cattia.r@bluemail.ch>
2013-07-31 16:22:41 +0200

    More points
    Added more points at different radii, and possibility to add points at
    different heights.

--- zylind_rot.py
+++ zylind_rot.py
@@ -2,13 +2,17 @@ import math
 
 r=1
 h=1
-steps=16
+angle_steps=16
+radius_steps=5
+height_steps=5
 
-for phi in range(steps+1):
-    x=r*math.cos(phi/(steps)*2*math.pi)
-    y=r*math.sin(phi/(steps)*2*math.pi)
-    z=h
-    point=[x,y,z]
+for phi in range(angle_steps+1):
+    for n in range(radius_steps):
+        r=(n+1)/radius_steps
+        x=r*math.cos(phi/(angle_steps)*2*math.pi)
+        y=r*math.sin(phi/(angle_steps)*2*math.pi)
+        z=h
+        point=[x,y,z]
 
-    print('({0:.3f},{1:.3f},{2:.3f})'.format(point[0],point[1],point[2]))
-    
+        print('({0:.3f},{1:.3f},{2:.3f})'.format(point[0],point[1],point[2]), end=',')
+    print(end='\n')
