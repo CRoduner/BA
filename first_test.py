8ff0df0f0170601cded18ceace9275349fe2c503
CRoduner <cattia.r@bluemail.ch>
2013-08-14 13:57:19 +0200

    Getting the grids ready for the discretization
    - added w-grid & adjusted grids to the one used in discretization
    -> only done for r yet
    - added stepsizes

--- first_test.py
+++ first_test.py
@@ -5,39 +5,43 @@ import matplotlib.pyplot as plt
 
 
 pi=math.pi
-# u Radialkomponente der Geschwindigkeit, v Azimutalkomponente der Geschwindigkeit, p Druck
+# u Radialkomponente der Geschwindigkeit, v Azimutalkomponente der Geschwindigkeit, w vertikale Komponente der Geschwindigkeit, p Druck
 # Tankgrenzen bzw. Tankraender
 r_t=10
 h_t=10
 
-# Anzahl Gitterpunkte
+# number of grid points
 n_r,n_phi,n_z = 50,50,25
 
-# p-Gitter p(r,phi,z):
-r_p = np.linspace(r_t/n_r,r_t, n_r)
-phi_p = np.linspace(pi/2,2*pi+pi/2, n_phi)
-z_p = np.linspace(0,h_t, n_z)
-
-p_grid = [r_p,phi_p,z_p]
+# stepsizes
+dr = r_t/n_r
+dphi = 2*pi/n_phi
+dz = h_t/n_z
 
 # u-Gitter u(r,phi,z):
-r_u = np.linspace(r_t/(2*n_r),r_t-r_t/(2*n_r), n_r)
+r_u = np.linspace(dr-dr/2,r_t-dr/2, n_r)
 phi_u = np.linspace(pi/2,2*pi+pi/2, n_phi)
 z_u = np.linspace(0,h_t, n_z)
 
 u_grid = [r_u,phi_u,z_u]
 
 # v-Gitter v(r,phi,z):
-r_v = np.linspace(r_t/n_r,r_t, n_r)
+r_v = np.linspace(dr,r_t, n_r)
 phi_v = np.linspace(0,2*pi, n_phi)
 z_v = np.linspace(0,h_t, n_z)
 
 v_grid = [r_v,phi_v,z_v]
 
-# stepsizes    -> linspace retstep
-dr =
-dphi
-dz
+# w-Gitter w(r,phi,z)
+
+
+# p-Gitter p(r,phi,z):
+r_p = np.linspace(dr,r_t, n_r)
+phi_p = np.linspace(pi/2,2*pi+pi/2, n_phi)
+z_p = np.linspace(0,h_t, n_z)
+
+p_grid = [r_p,phi_p,z_p]
+
 
 # matrix initialization
 u = np.zeros((50,50,25))
